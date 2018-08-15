a8fd827f6a1abbb507bce39277855087ed9d390f
cjgalvin <casey.j.galvin@gmail.com>
2018-08-15 14:55:44 +0900

    Update locatingDnaAboxes.py

--- locatingDnaAboxes.py
+++ locatingDnaAboxes.py
@@ -1,3 +1,24 @@
+def skew(Text):
+    """Finds location of minimum number of G relative to C basepairs in sequence
+    The starting point of DNA replication (ori) in a circular genome (e.g., bacteria) may be located near the minima"""
+    
+    skew_dict = {"A":0, "C":-1, "G":+1, "T":0}
+    skew_list = [0]
+        
+    for i, bp in enumerate(Text):
+        skew_list.append(skew_list[i] + skew_dict[bp])
+    
+    #Below return returns indexed values of skew
+    #return " ".join(map(str,skew_list))
+    
+    #Pre-calculate min_val to avoid additional O(L) calculation in list comprehension
+    min_val = min(skew_list)
+    
+    #Below return returns index for minimum skew values
+    min_skew_indexes = [j for j in range(len(skew_list)) if skew_list[j] == min_val]
+    print(" ".join(map(str, min_skew_indexes)))
+    
+    
 def ReverseComplement(Pattern):
     """Returns reverse complement of Pattern"""
     comp_bps = {"A":"T", "T":"A", "G":"C", "C":"G"}
@@ -7,6 +28,7 @@ def ReverseComplement(Pattern):
         comp_seq.insert(0,comp_bps[bp])
     
     return ''.join(comp_seq)
+
 
 def PatternToNumber(Pattern):
     """Returns a numeric code for Pattern according to base pair sequence in Pattern"""
@@ -20,6 +42,7 @@ def PatternToNumber(Pattern):
         prefix = Pattern[:len(Pattern)-1]
         return 4 * PatternToNumber(prefix) + sym_dict[symbol]
 
+    
 def NumberToPattern(number, k):
     """Returns the base pair sequence of length k based on number"""
     from itertools import product
@@ -32,8 +55,22 @@ def NumberToPattern(number, k):
 
     return [key for key, value in mer_dict.items() if value == number][0]    
 
+
+def HammingDistance(p, q):
+    """Calculates the Hamming Distance between two sequences p and q,
+    where the Hamming Distance is the number of basepair differences"""
+    d = 0
+    for i in range(len(p)):
+        if p[i] == q[i]:
+            continue
+        else:
+            d += 1
+    return d
+
+
 def Neighbors(Pattern, d):
-    """Generates a list of all kmers within a Hamming Distance <= d of Pattern"""
+    """Generates a list of all kmers within a Hamming Distance <= d of Pattern
+    This strategy is an alternative to generating ALL possible kmers regardless of d"""
     nts = ["A", "C", "G", "T"]
     
     #Exact match only case
@@ -66,10 +103,15 @@ def Neighbors(Pattern, d):
             neighborhood_list.append(Pattern[0] + text)
         
     return neighborhood_list
-    
+
+
 def FrequentWordsWithMismatchesAndReverseComplementsSorting(Text, k, d):
     """Finds most frequent k-mers and their reverse complements in Text with d mismatches or less
-    Implements a frequency array and neighbors sorting algorithm to achieve significant reduction in run time relative to nested loops"""
+    Finds all kmers in Text, followed by all kmers within HammingDistance d for kmers in text, 
+    and finally the reverse complement sequences of all the previously generated kmers
+    Transforms kmers into numeric pattern for sorting, then sorts and finds number of occurrences for each numeric pattern
+    Finally, takes the maximum frequency and returns all numeric codes with that frequency, followed by translation back to original sequences
+    The sorting algorithm achieves significant reduction in run time relative to nested loop algorithms"""
     
     import time
     
